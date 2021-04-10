9cdebe3b2cef4a692e6bac674dbf9ba8636bc5c2
Maksim Pischulenok <41614960+pischule@users.noreply.github.com>
2021-04-10 20:14:50 +0300

    Fix large ranges bug

--- randomizer/views.py
+++ randomizer/views.py
@@ -8,8 +8,7 @@ def randomize(request):
     try:
         start, end, count = int(request.GET['start']), int(request.GET['end']),\
                             int(request.GET['count'])
-        input_range = list(range(start,end))
-        output = random.choices(input_range,k=count)
+        output = random.choices(range(start, end), k=count)
         return render(request,'randomizer/index.html', {
             'range': input_range,
             'output': output,
